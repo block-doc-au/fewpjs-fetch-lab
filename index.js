82cdc0f9a127f61a6dc71f1eef796cc2adf4004c
Neil Cochrane <neilc@cns-gc.com.au>
2022-10-09 11:33:00 +1000

    NC completed Fetch lab

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+// const { returns } = require("chai-spies");
+
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(function(json){
+    // console.log(`The JSON returned is : ${json}.`);
+    return renderBooks(json);
+  })
 }
 
 function renderBooks(books) {
